18a18038123a262248ecbae285aeacdd9f535175
Sam Rutcovschi <samuil.rutcovschi@codwer.com>
2023-04-03 23:12:48 +0300

    change the way of getting client and server path

--- Socket1/Start.cs
+++ Socket1/Start.cs
@@ -1,16 +1,36 @@
 ﻿using System.Diagnostics;
+using System.Reflection;
+using System.Text;
 
+var curentPath = Assembly.GetEntryAssembly().Location;
 
-var serverFullPath = 
-    "D:\\Coding\\C#\\Retea\\SocketTCP\\Server\\bin\\Debug\\net6.0\\Server.exe";
+var arr = curentPath.Split('\\');
+var clientFullPath = new StringBuilder();
+var serverFullPath = new StringBuilder();
+for (var i = 0; i < arr.Length; i++)
+{
+    var item = arr[i];
+    if (i == arr.Length - 5)
+    {
+        clientFullPath.Append("Client\\\\");
+        serverFullPath.Append("Server\\\\");
+    }
+    else if (i < arr.Length - 1)
+    {
+        clientFullPath.Append($"{item}\\\\");
+        serverFullPath.Append($"{item}\\\\");
+    }
+    else
+    {
+        clientFullPath.Append("Client.exe");
+        serverFullPath.Append("Server.exe");
+    }
+}
 
-var clientFullPath = 
-    "D:\\Coding\\C#\\Retea\\SocketTCP\\Client\\bin\\Debug\\net6.0\\Client.exe";
-
-ProcessStartInfo infoServer = new ProcessStartInfo(serverFullPath);
+ProcessStartInfo infoServer = new ProcessStartInfo(serverFullPath.ToString());
 infoServer.UseShellExecute = true;
 
-ProcessStartInfo infoClient = new ProcessStartInfo(clientFullPath);
+ProcessStartInfo infoClient = new ProcessStartInfo(clientFullPath.ToString());
 infoClient.UseShellExecute = true;
 
 Process serverProcess = Process.Start(infoServer);
@@ -21,3 +41,4 @@ for (int i = 0; i < 2; i++)
 {
     Process clientProcess = Process.Start(infoClient);
 }
+
